d1e9afd8d8a13595a5cc26c6b811a7a03a7bfb40
bbs220 <bsawant357@gmail.com>
2025-07-16 20:19:14 +0530

    shaders

--- client/src/ts/testScene/cube.ts
+++ client/src/ts/testScene/cube.ts
@@ -1,7 +1,12 @@
 import * as THREE from "three";
+import testVert from "../../shaders/testVert.glsl";
+import testFrag from "../../shaders/testFrag.glsl";
 
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.ShaderMaterial({});
+const cubeGeometry = new THREE.SphereGeometry(0.5, 12, 12);
+const cubeMaterial = new THREE.ShaderMaterial({
+  fragmentShader: testFrag,
+  vertexShader: testVert,
+});
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 cubeMesh.castShadow = true;
 
