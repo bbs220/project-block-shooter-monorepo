3a553c28e1b3412bba99507ebd6c09772567a6ac
bbs220 <bsawant357@gmail.com>
2025-07-30 18:51:42 +0530

    killed off server

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "⭐ api is alive" });
+});
+
+app.listen(PORT, async () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
