96e60e49da455c98d9f4a41c9f6e8cbc1ae405d8
bbs220 <bsawant357@gmail.com>
2026-07-08 19:19:37 +0530

    cleanup server side

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,3 @@
-import express, { Application, Request, Response } from "express";
 import "dotenv/config";
-import cors from "cors";
 
-const app: Application = express();
-const PORT = process.env.PORT;
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "⭐ api is alive" });
-});
-
-app.listen(PORT, async () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+console.log(process.env);
