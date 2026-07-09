b0c1e86bc7e16e9b476f6d36469e7d4e7c6c3450
bbs220 <bsawant357@gmail.com>
2026-07-09 20:18:50 +0530

    minor fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 function App() {
   return (
-    <div className="font-inter overflow-x-hidden h-dvh w-full">
+    <div className="font-inter overflow-x-hidden h-full w-full">
       <Outlet />
     </div>
   );
